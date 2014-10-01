e0f4ef9f81b981b7f9019d4fc24803cb31a2170e
Laurent Tardif <Laurent.tardif@persistentsas.com>
2014-10-01 09:39:20 +0200

    Initial project structure

---
+++ Project1/module1/src/main/java/org/persistentsas/mavenTraining/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.persistentsas.mavenTraining ; 
+
+public class HelloWorld {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}	
+
+}
---
+++ Project1/module2/src/main/java/org/persistentsas/mavenTraining/HelloWorld2.java
@@ -0,0 +1,10 @@
+package org.persistentsas.mavenTraining ; 
+
+public class HelloWorld2 {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}	
+
+}
