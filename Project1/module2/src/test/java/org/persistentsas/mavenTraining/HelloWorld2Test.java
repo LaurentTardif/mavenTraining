ad1b7f1783ffb2cb88a21c97a757bf809f314519
Laurent Tardif <Laurent.tardif@persistentsas.com>
2014-10-01 15:11:42 +0200

    mvn verify ok

---
+++ Project1/module2/src/test/java/org/persistentsas/mavenTraining/HelloWorld2Test.java
@@ -0,0 +1,19 @@
+package org.persistentsas.mavenTraining ;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class HelloWorld2Test {
+
+	
+	/** very basic test for demo purpose */
+	@Test
+	public void testGetMessage() {
+		 HelloWorld2 underTest = new HelloWorld2("Mickey Mouse","Earth");
+		 Assert.assertTrue("The getMessage did not return what's expected : (" + underTest.getMessage()+") instead of ("+"Hello World, my name is Mickey Mouse and I come from Earth"+")",underTest.getMessage().toString().equals("Hello World, my name is Mickey Mouse and I come from Earth"));
+	}
+
+}
