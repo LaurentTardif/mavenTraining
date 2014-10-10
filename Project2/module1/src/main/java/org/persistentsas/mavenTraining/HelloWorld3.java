0236d8dd898e816bc40861835f929b52cffcb59a
Laurent Tardif <Laurent.tardif@persistentsas.com>
2014-10-10 15:09:45 +0200

    add translation in code in order to demonstrate L10N plugin

--- Project2/module1/src/main/java/org/persistentsas/mavenTraining/HelloWorld3.java
+++ Project2/module1/src/main/java/org/persistentsas/mavenTraining/HelloWorld3.java
@@ -1,10 +1,17 @@
 package org.persistentsas.mavenTraining ; 
 
+import java.util.Locale; 
+import java.util.ResourceBundle; 
+
 public class HelloWorld3 {
 
-
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Locale locale = Locale.getDefault();
+		System.out.println("Current locale is : " + locale);
+		ResourceBundle rb = ResourceBundle.getBundle("LanguageSpecific",locale);
+        String hello = rb.getString("Hello");
+		String world = rb.getString("World");
+		System.out.println(hello+" "+world);
 	}	
 
 }
